3d2245eddf07c38ace8d28c7d2ac2eba0c3ead8b
SemenVladymir <129590655+SemenVladymir@users.noreply.github.com>
2023-11-17 09:31:31 +0200

    Add files via upload

---
+++ Pixabay.DAL/Entities/FileModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Pixabay.DAL.Entities
+{
+
+    public class FileModel
+    {
+        public string FileName { get; set; }
+        public IFormFile FormFile { get; set;}
+    }
+}
---
+++ Pixabay.DAL/Entities/MyExceptionResponse.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Pixabay.DAL.Entities
+{
+    public class MyExceptionResponse : AbstractExceptionHandlerMiddleware
+    {
+        public MyExceptionResponse(RequestDelegate next) : base(next)
+        {
+        }
+        public override (HttpStatusCode code, string message) GetResponse(Exception exception)
+        {
+            HttpStatusCode code;
+            switch (exception)
+            {
+                case KeyNotFoundException
+                    or FileNotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+                case UnauthorizedAccessException:
+                    code = HttpStatusCode.Unauthorized;
+                    break;
+                case ArgumentException
+                    or InvalidOperationException:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+                default:
+                    code = HttpStatusCode.InternalServerError;
+                    break;
+            }
+            return (code, JsonConvert.SerializeObject(exception.Message));
+        }
+    }
+}

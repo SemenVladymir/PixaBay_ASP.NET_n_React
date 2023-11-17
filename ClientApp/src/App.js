b23abd86cd1abc8735081fb2b9a419244ee328d1
SemenVladymir <129590655+SemenVladymir@users.noreply.github.com>
2023-11-17 09:54:19 +0200

    Authorisation added

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,62 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OldApp from './Components/OldApp';
+import Registration from './Components/Registration';
 
-class App extends Component {
+export default function App() {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: []
-    }
-  }
-
-  API_URL = "http://localhost:5267/";
-
-  async addClick() {
-    const data = new FormData();
-    data.append("Name", document.getElementById("search").value);
-    data.append("UrlPhoto", "non");
-    data.append("Size", document.getElementById("Number").value);
-
-
-    fetch(this.API_URL + "api/Image/GetImages", {
-      method: "POST",
-      body: data
-    }).then(res => res.json()).then((result) => {
-      alert(result);
-      this.setState({ images: result });
-    })
-  }
-
-  render() {
-    const { images } = this.state;
-    
-  
     return (
-      <div className="App">
-        <header>
-          <div className='head'>
-          <h2>Searching image on the pixabay</h2>
-          <label> Search name images </label>
-          <input id="search" />&nbsp;
-          
-          <label> Number of images </label>
-          <input id="Number" />&nbsp;
-          
-            <button onClick={() => this.addClick()}>Search</button>
-            </div>
-        </header>
-        <body>
-            <div className='container'>
-              {images.map(image =>
-                <div className='item-content'>
-                  <img src={image.UrlPhoto} alt="pixabay"></img>
-                </div>
-              )}
-            </div>
-        </body>
+      <div>
+        
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<OldApp />} />
+          <Route path="/login" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
+      
       </div>
     );
-  }
 }
-export default App;

b1996e350bc65d9593e754268481634463bb5d89
SemenVladymir <129590655+SemenVladymir@users.noreply.github.com>
2023-11-17 09:30:09 +0200

    Add files via upload

---
+++ Pixabay.DAL/EF/AuthContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Pixabay.DAL.Entities;
+
+namespace Pixabay.DAL.EF
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+
+        public DbSet<MyImage> Images { get; set; }
+        public AuthContext(DbContextOptions<AuthContext> options)
+            : base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
